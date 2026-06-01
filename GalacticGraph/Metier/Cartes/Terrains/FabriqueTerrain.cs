6c3943e12dfb396445284810a6336ca3354ca67c
Tiph <arquetout.tiphanie@icloud.com>
2026-06-01 21:47:39 +0200

    FabriqueTerrain Terrain

---
+++ GalacticGraph/Metier/Cartes/Terrains/FabriqueTerrain.cs
@@ -0,0 +1,30 @@
+﻿using GalacticGraph.Metier.Cartes.Terrains.Realisations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticGraph.Metier.Cartes.Terrains
+{
+    public class FabriqueTerrain
+    {
+        /// <summary>
+        /// Crée un terrain en fonction du caractère reçu du serveur
+        /// </summary>
+        /// <param name="caractere">Caractère représentant le type de terrain (protocole réseau)</param>
+        /// <returns>Un terrain du bon type</returns>
+        public static Terrain Creer(char caractere)
+        {
+            switch (caractere)
+            {
+                case 'V': return new TerrainVide();
+                case 'A': return new TerrainAsteroides();
+                case 'B': return new TerrainBase();
+                case 'S': return new TerrainSoleil();
+                case 'P': return new TerrainPlanete();
+                default: return new TerrainInconnu(); 
+            }
+        }
+    }
+}

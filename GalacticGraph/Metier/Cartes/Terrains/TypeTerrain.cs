bc4b34e9b374fe09a50e642d8cf7879c58f7efde
Tiph <arquetout.tiphanie@icloud.com>
2026-06-01 21:40:16 +0200

    Realisations TypeTerrain Question 12

---
+++ GalacticGraph/Metier/Cartes/Terrains/TypeTerrain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticGraph.Metier.Cartes.Terrains
+{
+    /// <summary>
+    /// Enumération représentant les différents types de terrain
+    /// </summary>
+    public enum TypeTerrain
+    {
+        VIDE,
+        ASTEROIDES,
+        BASE,
+        SOLEIL,
+        PLANETE,
+        INCONNU
+    }
+}

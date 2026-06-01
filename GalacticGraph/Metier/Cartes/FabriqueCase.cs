b34587925a923a1395dc17445631ae388d059de0
Tiph <arquetout.tiphanie@icloud.com>
2026-06-01 21:49:44 +0200

    FabriqueCase Case

---
+++ GalacticGraph/Metier/Cartes/FabriqueCase.cs
@@ -0,0 +1,27 @@
+﻿using GalacticGraph.Metier.Cartes.Terrains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticGraph.Metier.Cartes
+{
+    /// <summary>
+    /// Fabrique permettant de créer une case à partir d'un caractère
+    /// </summary>
+    public class FabriqueCase
+    {
+        /// <summary>
+        /// Crée une case en fonction du caractère reçu du serveur
+        /// </summary>
+        /// <param name="caractere">Le caractère représentant le type de case</param>
+        /// <returns>Une case avec le bon terrain</returns>
+        public static Case Creer(char caractere)
+        {
+            Terrain terrain = FabriqueTerrain.Creer(caractere);
+            Case laCase = new Case(terrain);
+            return laCase;
+        }
+    }
+}

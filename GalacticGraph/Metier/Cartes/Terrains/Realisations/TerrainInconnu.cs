11a35eb7ae773d430881b09982be283e0cabf2d2
Tiph <arquetout.tiphanie@icloud.com>
2026-06-01 21:36:52 +0200

    Realisations Question 11

---
+++ GalacticGraph/Metier/Cartes/Terrains/Realisations/TerrainInconnu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticGraph.Metier.Cartes.Terrains.Realisations
+{
+    internal class TerrainInconnu
+    {
+    }
+}

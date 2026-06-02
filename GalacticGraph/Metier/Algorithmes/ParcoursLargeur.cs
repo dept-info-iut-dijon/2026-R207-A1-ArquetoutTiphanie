bbcc8cb003b779924454138ca45bbe4a7870e238
Tiph <arquetout.tiphanie@icloud.com>
2026-06-02 14:41:33 +0200

    Question 9

---
+++ GalacticGraph/Metier/Algorithmes/ParcoursLargeur.cs
@@ -0,0 +1,57 @@
+﻿using GalacticGraph.Metier.Cartes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticGraph.Metier.Algorithmes
+{
+    public class ParcoursLargeur : AlgorithmeCalculDistance
+    {
+        #region --- Constructeur ---
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
+        /// <param name="carte">La carte du jeu</param>
+        public ParcoursLargeur(Carte carte) : base(carte)
+        {
+        }
+        #endregion
+
+        #region --- Méthodes ---
+        /// <summary>
+        /// Calcule les distances depuis la case de départ par parcours en largeur
+        /// </summary>
+        /// <param name="depart">La case de départ</param>
+        public override void CalculerDistancesDepuis(Case depart)
+        {
+            //remettre à zéro les distances
+            List<Case> aTraiter = new List<Case>();
+            this.ReinitialisationDistances();
+
+            //initilisation de la distance de la case de départ à 0
+            aTraiter.Add(depart);
+            this.SetDistance(depart, 0);
+
+            while (aTraiter.Count > 0)
+            {
+                Case caseEnCours = aTraiter[0];
+                aTraiter.RemoveAt(0);
+
+                // On parcourt tous les voisins
+                foreach (Case v in caseEnCours.Voisins)
+                {
+                    if (this.GetDistance(v) == -1 && v.EstAccessible)
+                    {
+                        this.SetDistance(v, this.GetDistance(caseEnCours) + 1);
+                        aTraiter.Add(v);
+                    }
+                }
+
+            }
+            
+        }
+        #endregion
+    }
+}
---
+++ IATest/TestParcoursLargeur.cs
@@ -0,0 +1,124 @@
+﻿using GalacticGraph.Metier.Algorithmes;
+using GalacticGraph.Metier.Cartes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IATest
+{
+    public class TestParcoursLargeur
+    {
+
+        private string FinDeLigne()
+        {
+            return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        }
+
+        private string Ligne()
+        {
+            return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        }
+
+
+
+
+        [Fact]
+        public void TestVideAsteroides()
+        {
+            string messageCarte = "";
+            messageCarte += "AAAAAAAAAA" + FinDeLigne();
+            messageCarte += "AVAAVVVVAA" + FinDeLigne();
+            messageCarte += "AVAAAAAAVA" + FinDeLigne();
+            messageCarte += "AVAAAAAAVA" + FinDeLigne();
+            messageCarte += "AAVVVVVVVA" + FinDeLigne();
+            messageCarte += "AAAAAAAAAA" + FinDeLigne();
+            for (int i = 6; i < 40; i++) messageCarte += Ligne();
+
+            Carte carte = new Carte(messageCarte);
+
+            ParcoursLargeur parcours = new ParcoursLargeur(carte);
+            parcours.CalculerDistancesDepuis(carte.GetCaseAt(new Coordonnees(1, 1)));
+
+            int[,] expectedResults =
+            {
+                {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+                {-1, 0,-1,-1,15,14,13,12,-1,-1},
+                {-1, 1,-1,-1,-1,-1,-1,-1,11,-1},
+                {-1, 2,-1,-1,-1,-1,-1,-1,10,-1},
+                {-1,-1, 3, 4, 5, 6, 7, 8, 9,-1},
+                {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+            };
+
+            for (int i = 0; i < 6; i++)
+                for (int j = 0; j < 10; j++)
+                    Assert.Equal(expectedResults[i, j], parcours.GetDistance(carte.GetCaseAt(new Coordonnees(i, j))));
+        }
+
+        [Fact]
+        public void TestBaseEtPlanete()
+        {
+            string messageCarte = "";
+            messageCarte += "AAAAAAAAAA" + FinDeLigne();
+            messageCarte += "AVAAVVVVAA" + FinDeLigne();
+            messageCarte += "ABAAAAAAVA" + FinDeLigne();
+            messageCarte += "AVAAAAAAVA" + FinDeLigne();
+            messageCarte += "AAPPPPPPPA" + FinDeLigne();
+            messageCarte += "AAAAAAAAAA" + FinDeLigne();
+            for (int i = 6; i < 40; i++) messageCarte += Ligne();
+
+            Carte carte = new Carte(messageCarte);
+
+            ParcoursLargeur parcours = new ParcoursLargeur(carte);
+            parcours.CalculerDistancesDepuis(carte.GetCaseAt(new Coordonnees(1, 1)));
+
+            int[,] expectedResults =
+            {
+                {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+                {-1, 0,-1,-1,15,14,13,12,-1,-1},
+                {-1, 1,-1,-1,-1,-1,-1,-1,11,-1},
+                {-1, 2,-1,-1,-1,-1,-1,-1,10,-1},
+                {-1,-1, 3, 4, 5, 6, 7, 8, 9,-1},
+                {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+            };
+
+            for (int i = 0; i < 6; i++)
+                for (int j = 0; j < 10; j++)
+                    Assert.Equal(expectedResults[i, j], parcours.GetDistance(carte.GetCaseAt(new Coordonnees(i, j))));
+        }
+
+        [Fact]
+        public void TestInconnuEtSoleil()
+        {
+            string messageCarte = "";
+            messageCarte += "AAAAAAAAAA" + FinDeLigne();
+            messageCarte += "AVAAVVVVAA" + FinDeLigne();
+            messageCarte += "AVAAAAAASA" + FinDeLigne();
+            messageCarte += "AVAAAAAAVA" + FinDeLigne();
+            messageCarte += "AAXXXXXXXA" + FinDeLigne();
+            messageCarte += "AAAAAAAAAA" + FinDeLigne();
+            for (int i = 6; i < 40; i++) messageCarte += Ligne();
+
+            Carte carte = new Carte(messageCarte);
+
+            ParcoursLargeur parcours = new ParcoursLargeur(carte);
+            parcours.CalculerDistancesDepuis(carte.GetCaseAt(new Coordonnees(1, 1)));
+
+            int[,] expectedResults =
+            {
+                {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+                {-1, 0,-1,-1,-1,-1,-1,-1,-1,-1},
+                {-1, 1,-1,-1,-1,-1,-1,-1,-1,-1},
+                {-1, 2,-1,-1,-1,-1,-1,-1,10,-1},
+                {-1,-1, 3, 4, 5, 6, 7, 8, 9,-1},
+                {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+            };
+
+            for (int i = 0; i < 6; i++)
+                for (int j = 0; j < 10; j++)
+                    Assert.Equal(expectedResults[i, j], parcours.GetDistance(carte.GetCaseAt(new Coordonnees(i, j))));
+        }
+    }
+}
+

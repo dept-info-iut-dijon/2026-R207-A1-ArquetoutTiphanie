6bd745f817dac294489b6b64435f9098e631eadb
Tiph <arquetout.tiphanie@icloud.com>
2026-06-18 18:09:12 +0200

    TP5: Q5

--- GalacticGraph/Metier/Vaisseau.cs
+++ GalacticGraph/Metier/Vaisseau.cs
@@ -14,6 +14,7 @@ namespace GalacticGraph.Metier
     {
         #region --- Attributs ---
         private Coordonnees coordonnees;
+        private List<Direction> ordres;
         #endregion
 
         #region --- Propriétés ---
@@ -21,6 +22,14 @@ namespace GalacticGraph.Metier
         /// Coordonnées actuelles du vaisseau sur la carte
         /// </summary>
         public Coordonnees Coordonnees => this.coordonnees;
+
+        /// <summary>
+        /// Indique si le vaisseau a des ordres à réaliser
+        /// </summary>
+        public bool HasOrdres
+        {
+            get { return this.ordres.Count > 0; }
+        }
         #endregion
 
         #region --- Constructeur ---
@@ -31,6 +40,39 @@ namespace GalacticGraph.Metier
         public Vaisseau(Coordonnees coordonnees)
         {
             this.coordonnees = coordonnees;
+            this.ordres = new List<Direction>();
+        }
+        #endregion
+
+        #region --- Méthodes ---
+        /// <summary>
+        /// Ajoute une liste d'ordres à réaliser au vaisseau
+        /// </summary>
+        /// <param name="nouveauxOrdres">La liste des ordres à ajouter</param>
+        public void AjouterOrdres(List<Direction> nouveauxOrdres)
+        {
+            this.ordres.AddRange(nouveauxOrdres);
+        }
+
+        /// <summary>
+        /// Déplace le vaisseau dans la direction donnée
+        /// </summary>
+        /// <param name="direction">La direction du déplacement</param>
+        private void Deplacer(Direction direction)
+        {
+            this.coordonnees = this.coordonnees.GetVoisin(direction);
+        }
+
+        /// <summary>
+        /// Exécute le premier ordre, le retire de la liste et renvoie la direction réalisée
+        /// </summary>
+        /// <returns>La direction exécutée</returns>
+        public Direction ExecuterOrdre()
+        {
+            Direction direction = this.ordres[0];
+            this.ordres.RemoveAt(0);
+            this.Deplacer(direction);
+            return direction;
         }
         #endregion
     }
